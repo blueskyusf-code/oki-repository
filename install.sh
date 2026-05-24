41e114dffac974e922e2ee1d9ebdf2d2bff46e71
Bluesky <blueskyusf@gmail.com>
2026-05-24 19:32:41 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 echo "OKI | Criado por Turma do The Blu3sky"
 echo "[*] Instalando pelo curl..."
 sleep 2
-curl -sL "https://raw.githubusercontent.com/blueskyusf-code/oki-repository/main/packages/oki/oki" -o $PREFIX/bin/oki && chmod +x $PREFIX/bin/oki
+curl -sL "https://raw.githubusercontent.com/blueskyusf-code/oki-repository/main/oki" -o $PREFIX/bin/oki && chmod +x $PREFIX/bin/oki
